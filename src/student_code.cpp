4cd4de6b0cc2cda7688adeb458eef5a7175008d8
gabrielledelforge <gabrielle.delforge@berkeley.edu>
2020-02-18 11:19:26 -0800

    Add default return values

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -5,58 +5,64 @@ using namespace std;
 
 namespace CGL
 {
-  void BezierCurve::evaluateStep()
-  {
-    // TODO Part 1.
-    // Perform one step of the Bezier curve's evaluation at t using de Casteljau's algorithm for subdivision.
-    // Store all of the intermediate control points into the 2D vector evaluatedLevels.
+    void BezierCurve::evaluateStep()
+    {
+        // TODO Part 1.
+        // Perform one step of the Bezier curve's evaluation at t using de Casteljau's algorithm for subdivision.
+        // Store all of the intermediate control points into the 2D vector evaluatedLevels.
+        return;
 
-  }
+    }
 
-  Vector3D BezierPatch::evaluate(double u, double v) const
-  {
-    // TODO Part 2.
-    // Evaluate the Bezier surface at parameters (u, v) through 2D de Casteljau subdivision.
-    // (i.e. Unlike Part 1 where we performed one subdivision level per call to evaluateStep, this function
-    // should apply de Casteljau's algorithm until it computes the final, evaluated point on the surface)
+    Vector3D BezierPatch::evaluate(double u, double v) const
+    {
+        // TODO Part 2.
+        // Evaluate the Bezier surface at parameters (u, v) through 2D de Casteljau subdivision.
+        // (i.e. Unlike Part 1 where we performed one subdivision level per call to evaluateStep, this function
+        // should apply de Casteljau's algorithm until it computes the final, evaluated point on the surface)
+        return Vector3D();
 
-  }
+    }
 
-  Vector3D BezierPatch::evaluate1D(std::vector<Vector3D> points, double t) const
-  {
-    // TODO Part 2.
-    // Optional helper function that you might find useful to implement as an abstraction when implementing BezierPatch::evaluate.
-    // Given an array of 4 points that lie on a single curve, evaluates the Bezier curve at parameter t using 1D de Casteljau subdivision.
+    Vector3D BezierPatch::evaluate1D(std::vector<Vector3D> points, double t) const
+    {
+        // TODO Part 2.
+        // Optional helper function that you might find useful to implement as an abstraction when implementing BezierPatch::evaluate.
+        // Given an array of 4 points that lie on a single curve, evaluates the Bezier curve at parameter t using 1D de Casteljau subdivision.
+        return Vector3D();
+    }
 
-  }
+    Vector3D Vertex::normal(void) const
+    {
+        // TODO Part 3.
+        // Returns an approximate unit normal at this vertex, computed by
+        // taking the area-weighted average of the normals of neighboring
+        // triangles, then normalizing.
+        return Vector3D();
 
-  Vector3D Vertex::normal( void ) const
-  {
-    // TODO Part 3.
-    // Returns an approximate unit normal at this vertex, computed by
-    // taking the area-weighted average of the normals of neighboring
-    // triangles, then normalizing.
+    }
 
-  }
+    EdgeIter HalfedgeMesh::flipEdge(EdgeIter e0)
+    {
+        // TODO Part 4.
+        // This method should flip the given edge and return an iterator to the flipped edge.
+        return e0;
 
-  EdgeIter HalfedgeMesh::flipEdge( EdgeIter e0 )
-  {
-    // TODO Part 4.
-    // This method should flip the given edge and return an iterator to the flipped edge.
+    }
 
-  }
+    VertexIter HalfedgeMesh::splitEdge(EdgeIter e0)
+    {
+        // TODO Part 5.
+        // This method should split the given edge and return an iterator to the newly inserted vertex.
+        // The halfedge of this vertex should point along the edge that was split, rather than the new edges.
+        return VertexIter();
+    }
 
-  VertexIter HalfedgeMesh::splitEdge( EdgeIter e0 )
-  {
-    // TODO Part 5.
-    // This method should split the given edge and return an iterator to the newly inserted vertex.
-    // The halfedge of this vertex should point along the edge that was split, rather than the new edges.
+    void MeshResampler::upsample(HalfedgeMesh& mesh)
+    {
+        // TODO Part 6.
+        // This routine should increase the number of triangles in the mesh using Loop subdivision.
+        return;
 
-  }
-
-  void MeshResampler::upsample( HalfedgeMesh& mesh )
-  {
-    // TODO Part 6.
-    // This routine should increase the number of triangles in the mesh using Loop subdivision.
-
-  }
+    }
+}
